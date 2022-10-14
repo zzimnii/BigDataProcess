612f969b5182ff2b8478212a008b3baa6baed25b
zzimnii <happykeil@naver.com>
2022-10-14 14:38:44 +0900

    py upload

--- HW2/student20200958.py
+++ HW2/student20200958.py
@@ -5,6 +5,7 @@ import openpyxl
 wb = openpyxl.load_workbook("student.xlsx")
 ws = wb['Sheet1']
 
+total = []
 row_id = 1;
 for row in ws:
 	if row_id != 1:
@@ -13,8 +14,11 @@ for row in ws:
 		sum_v += ws.cell(row = row_id, column = 5).value * 0.34
 		sum_v += ws.cell(row = row_id, column = 6).value
 		ws.cell(row = row_id, column = 7).value = sum_v
+		total.append(ws.cell(row = row_id, column = 7).value)
 	row_id += 1
 #여기까지는 total값 구하는 코드
 
+total.sort()
+print(total)
 
 wb.save("student.xlsx")
